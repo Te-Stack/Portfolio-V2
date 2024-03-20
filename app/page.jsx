89b216603960cce2713cc132ec4b3fe350292832
Te-Stack <ukumakubequincy@gmail.com>
2024-03-20 22:05:39 +0100

    Working on it

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <main >
+      <div>
+        This is the starting phase of my portfolio website
+      </div>
+    </main>
+  )
+}
