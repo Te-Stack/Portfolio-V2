84c25fdad14f50851996b6ad1cdf4a089dbb1125
Te-Stack <ukumakubequincy@gmail.com>
2024-03-23 19:22:40 +0100

    Setting Lenis Smooth Scroll

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import SmoothScrolling from "./components/SmoothScrolling";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SmoothScrolling>
+        {children}
+        </SmoothScrolling>
+        </body>
     </html>
   )
 }
