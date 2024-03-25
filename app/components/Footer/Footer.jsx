1f2b7539969c8d31a412c4c32d9df03d73c44811
Te-Stack <ukumakubequincy@gmail.com>
2024-03-25 11:45:24 +0100

    Creating the footer

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import "./footer.css"
-
+import { FaLinkedin, FaGithubSquare, FaTwitter,FaFreeCodeCamp  } from "react-icons/fa";
+import { FaHashnode } from "react-icons/fa6";
 const Footer = () => {
     return ( 
         <footer>
@@ -10,11 +11,10 @@ const Footer = () => {
             <h1 className="footer-logo">Quincy Oghenetejiri</h1>
             </div>
             <div className="con">
-                <Link href="https://wa.link/6yvpoe" target="_blank" rel="noreferrer"><i className="fab fa-whatsapp mx-2"></i></Link>
-                <Link href="https://www.linkedin.com/in/quincy-oghenetejiri"><i className="fab fa-linkedin mx-2" target="_blank" rel="noreferrer"></i></Link>
-                <Link href="https://www.github.com/Te-Stack" target="_blank" rel="noreferrer"><i className="fab fa-github mx-2"></i></Link>
-                <Link href="https://www.twitter.com/Quincyoghenex" target="_blank" rel="noreferrer"><i className="fab fa-twitter mx-2"></i></Link>
-                <Link href="https://quincyoghenetejiri.hashnode.dev/" target="_blank" rel="noreferrer"><i className="fab fas fa-blog mx-2"></i></Link>
+                <Link href="https://www.linkedin.com/in/quincy-oghenetejiri"><span><FaLinkedin /></span>Linkedin</Link>
+                <Link href="https://www.github.com/Te-Stack" target="_blank" rel="noreferrer"><FaGithubSquare /> GitHub</Link>
+                <Link href="https://www.twitter.com/Quincyoghenex" target="_blank" rel="noreferrer"><FaTwitter/> twitter</Link>
+                <Link href="https://quincyoghenetejiri.hashnode.dev/" target="_blank" rel="noreferrer"> <FaHashnode/> hashnode</Link>
             </div>
             <div>
                 <p>&copy; Copyright 2021, All right reserved</p>
